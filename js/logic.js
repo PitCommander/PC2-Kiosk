db029ae5e225e1b713c6949b689af29eb3561cd1
Preston <childrep@vt.edu>
2019-05-21 15:13:55 -0400

    Basic checklist functionality works

--- js/logic.js
+++ js/logic.js
@@ -27,15 +27,76 @@ let tvTemplate = Handlebars.compile(tvSource);
 let tvs = {
   'tv': ['Left', 'Right', 'Inner']
 };
-
 $('#tvHolder').append(tvTemplate(tvs));
-
 
 //Event listener for screen controls, sends data to rethink
 $('label.btn.orange-btn').click(function () {
   sendScreenUpdate($(this)[0].parentElement.id, $(this)[0].id);
 })
 
+//Templating for match and safety checklists
+let listSource = document.getElementById("checklist-template").innerHTML;
+let listTemplate = Handlebars.compile(listSource);
+let matchList = {
+  'listItem': [{
+      'type': 'match',
+      'text': 'Test functionality on all subsystems'
+    },
+    {
+      'type': 'match',
+      'text': 'Check all drivetrain chains for backlash'
+    },
+    {
+      'type': 'match',
+      'text': 'Fully pressurize pneumatic system'
+    },
+    {
+      'type': 'match',
+      'text': 'Ensure bumper color is correct for next match'
+    },
+    {
+      'type': 'match',
+      'text': 'Swap battery with fresh one'
+    },
+    {
+      'type': 'match',
+      'text': 'Secure battery strap'
+    },
+    {
+      'type': 'match',
+      'text': 'Double check battery connector'
+    },
+    {
+      'type': 'match',
+      'text': 'Recheck air pressure'
+    }]
+}
+
+let safetyList = {
+  'listItem': [{
+      'type': 'safety',
+      'text': 'All tools are put away'
+    },
+    {
+      'type': 'safety',
+      'text': 'No parts or scrap material is left out'
+    },
+    {
+      'type': 'safety',
+      'text': 'No more than six people are currently in the pit'
+    },
+    {
+      'type': 'safety',
+      'text': 'All people in the pit are wearing appropriate personal protection'
+    },
+    {
+      'type': 'safety',
+      'text': 'There are no shavings or trash in the pit'
+    }]
+}
+
+$('#matchHolder').append(listTemplate(matchList));
+$('#safetyHolder').append(listTemplate(safetyList));
 
 //Functions for sending and receiving from rethink
 function updateSchedule(err, scheduleData) {
