942717a6e0a5e201db764e4b18759a48c6d7357a
Preston <childrep@vt.edu>
2019-05-21 13:22:46 -0400

    Sending to rethink works.

--- js/logic.js
+++ js/logic.js
@@ -2,11 +2,15 @@
 let settings = require('./settings.json');
 let r = require('rethinkdb');
 
+var connection = null;
+
 //This section handles all connections to rethink
 r.connect({
   host: settings.serverAddress,
   port: settings.serverPort
 }, function (err, connection) {
+  window.connection = connection;
+
   //Runs when the schedule row updates
   r.db(settings.db).table(settings.table).get('schedule').changes({
     includeInitial: true
@@ -15,9 +19,35 @@ r.connect({
   });
 });
 
+//Templating stuff for element layouts
+let Handlebars = require('handlebars');
+let source = document.getElementById("tv-template").innerHTML;
+let template = Handlebars.compile(source);
+
+let tvs = {
+  'tv': ['Left', 'Right', 'Inner']
+};
+
+let result = template(tvs);
+$('#tvHolder').append(result);
+
+$('label.btn.orange-btn').click(function () {
+  sendScreenUpdate($(this)[0].parentElement.id, $(this)[0].id);
+})
+
+
+//Functions for sending and receiving from rethink
 function updateSchedule(err, scheduleData) {
   let table = $('#scheduleTable').DataTable();
   table.clear();
   table.rows.add(scheduleData.new_val.value);
   table.draw();
+}
+
+function sendScreenUpdate(screen, page) {
+  r.db(settings.db).table(settings.table).get('screenSettings').update({
+    "value": {
+      [screen]: page
+    }
+  }).run(window.connection, function (err, res) {});
 }
--- js/tv-item.js
+++
@@ -1,9 +0,0 @@
-let Handlebars = require('handlebars');
-let source = document.getElementById("tv-template").innerHTML;
-let template = Handlebars.compile(source);
-
-let tvs = {
-  'tv': ['Left', 'Right', 'Inner']
-};
-let result = template(tvs);
-$('#tvHolder').append(result);
